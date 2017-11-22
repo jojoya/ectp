40d6906821fc72908e07aa78a64f93db099c89c9
caixiaoling <cai0ling@163.com>
2017-11-22 11:28:41 +0800

    修改路径

--- src/main/java/com/workec/ectp/controller/ModuleController.java
+++ src/main/java/com/workec/ectp/controller/ModuleController.java
@@ -17,49 +17,49 @@ public class ModuleController {
     ModuleService moduleService;
 
     /* 查询模块列表 */
-    @GetMapping(value = "/getModuleList")
+    @GetMapping(value = "/module/getModuleList")
     public Result<Module> getModuleList() throws Exception {
         return moduleService.getModuleList();
     }
 
     /* 添加模块 */
-    @PostMapping(value = "/addModule")
+    @PostMapping(value = "/module/addModule")
     public Result<Module> addModule(@Valid Module module, BindingResult bindingResult){
         return moduleService.addModule(module,bindingResult);
     }
 
     /* 修改模块 */
-    @PostMapping(value = "/updateModule")
+    @PostMapping(value = "/module/updateModule")
     public Result<Module> updateModule(@Valid Module module, BindingResult bindingResult){
         return moduleService.updateModule(module,bindingResult);
     }
 
     /* 删除模块 */
-    @PostMapping(value = "/deleteModule")
+    @PostMapping(value = "/module/deleteModule")
     public Result<Module> deleteModuleById(@RequestParam("id") Integer id) throws Exception {
         return moduleService.deleteModuleById(id);
     }
 
     /* 按照id查询模块 */
-    @GetMapping(value = "/findModuleById/{id}")
+    @GetMapping(value = "/module/findModuleById/{id}")
     public Result<Module> findModuleById(@PathVariable("id") Integer id) throws Exception {
         return moduleService.findModuleById(id);
     }
 
     /* 根据当前节点ID统计直属子节点数量 */
-    @GetMapping(value = "/findChildrenCountByParentId/{id}")
+    @GetMapping(value = "/module/findChildrenCountByParentId/{id}")
     public Result<Module> findChildrenCountByParentId(@PathVariable("id") Integer id) throws Exception {
         return moduleService.findChildrenCountByParentId(id);
     }
 
     /* 根据当前节点ID查询直属子节点信息 */
-    @GetMapping(value = "/findChildrenByParentId/{id}")
+    @GetMapping(value = "/module/findChildrenByParentId/{id}")
     public Result<Module> findChildrenByParentId(@PathVariable("id") Integer id) throws Exception {
         return moduleService.findChildrenByParentId(id);
     }
 
     /* 按照name查询模块 */
-    @GetMapping(value = "/findModuleListByName/{name}")
+    @GetMapping(value = "/module/findModuleListByName/{name}")
     public Result<Module> findModuleListByName(@PathVariable("name") String name) throws Exception {
         return moduleService.findModuleListByName(name);
     }
