cdccfcd38ee584ffcfb5302eef336ed1364d14e4
caixiaoling <cai0ling@163.com>
2018-03-16 19:49:06 +0800

    调试接口、用例执行接口暴露了执行用户和执行环境信息

--- src/main/java/com/workec/ectp/entity/Bo/ExecuteOneCaseInputParams.java
+++ src/main/java/com/workec/ectp/entity/Bo/ExecuteOneCaseInputParams.java
@@ -2,20 +2,13 @@ package com.workec.ectp.entity.Bo;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * Created by user on 2018/3/16.
  */
 @Data
 public class ExecuteOneCaseInputParams {
 
-    @NotNull(message = "caseId不能为空")
     private Integer caseId;
-
-    @NotNull(message = "applicationEnvironmentId不能为空")
     private Integer applicationEnvironmentId;
-
-    @NotNull(message = "executeUserId不能为空")
     private Integer executeUserId;
 }
